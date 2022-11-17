a0ccecf8163e865f366fca7e926dad2c76452d4b
Om Suresh Javir <javirom07042003@gmail.com>
2022-11-17 16:24:38 +0530

    Day 9

--- questions.js
+++ questions.js
@@ -295,3 +295,45 @@
 //     }
 //     return result;
 // };
+
+
+
+// // ************* Day 9 **************
+// // Valid Sudoku 
+// var isValidSudoku = function (board) {
+//     let set = new Set()
+
+//     for (let i = 0; i < board.length; i++) {
+//         for (let j = 0; j < board[0].length; j++) {
+//             const value = board[i][j]
+//             if (value !== ".") {
+//                 const row = `${value} at row ${i}`
+//                 const column = `${value} at column ${j}`
+//                 const box = `${value} at box ${Math.floor(i / 3)}, ${Math.floor(j / 3)}`
+//                 console.log(`${row}, ${column}, ${box}`)
+//                 if (set.has(row) || set.has(column) || set.has(box)) {
+//                     return false
+//                 } else {
+//                     set.add(row)
+//                     set.add(column)
+//                     set.add(box)
+//                 }
+//             }
+//         }
+//     }
+
+//     return true
+// };
+
+
+// // Search a 2d Matrix
+// var searchMatrix = function (M, T) {
+//     let y = M.length, i = 0, j = M[0].length - 1
+//     while (i < y && ~j) {
+//         let cell = M[i][j]
+//         if (cell === T) return true
+//         else if (cell > T) j--
+//         else i++
+//     }
+//     return false
+// };
